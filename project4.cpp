fa5f095e146eda0f39dcc257c25d54a496097a75
chavranek <havranek@sonoma.edu>
2018-11-01 22:47:53 -0700

    evaluation function and status check

--- project4.cpp
+++ project4.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <iomanip>
 #include <vector>
+#include <numeric>
 using namespace std;
 
 string getFile(string fileName)
@@ -53,12 +54,146 @@ void printResults(float time)
     cout << endl;
 }
 
+float eval(vector< vector<string> > board)
+{   // this function looks at the current board state and returns 1 if
+    // its a win for player one, a 0 if its a win for player 2, 0.5
+    // if it's a tie and a -1 if it isn't a valid board.
+    int ones = 0;
+    int twos = 0;
+    int dif = 0;
+
+    // vectors to indicate whether a row or column has been filled for a given number
+    vector<int> rowP1 (4);
+    vector<int> colP1 (4);
+    vector<int> rowP2 (4);
+    vector<int> colP2 (4);
+
+    // variables to hold the total value of each of the above vectors
+    int rowP1Total = 0;
+    int colP1Total = 0;
+    int rowP2Total = 0;
+    int colP2Total = 0;
+
+    // booleans to indicate if a player has enough columns and rows to win
+    bool p1Done;
+    bool p2Done;
+
+    // for loop that iterates through the board and finds 2s and 1s.
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            string curVal = board[i][j];
+            if(curVal == "1")
+            {
+                ones++;
+                rowP1[i] = 1;
+                colP1[j] = 1;
+            }
+            else if(curVal == "2")
+            {
+                twos++;
+                rowP2[i] = 1;
+                colP2[j] = 1;
+            }
+        }
+    }
+
+    if(ones > twos)
+    {
+        dif = ones - twos;
+    }
+    else
+    {
+        dif = twos - ones;
+    }
+
+    // difference between amount of ones and twos is too great
+    // and therefore it is not a valid board.
+    if(dif > 1)
+    {
+        return -1;
+    }
+
+    // total up the values of the vectors
+    rowP1Total = accumulate(rowP1.begin(), rowP1.end(), 0);
+    colP1Total = accumulate(colP1.begin(), colP1.end(), 0);
+    rowP2Total = accumulate(rowP2.begin(), rowP2.end(), 0);
+    colP2Total = accumulate(colP2.begin(), colP2.end(), 0);
+
+    if((rowP1Total + colP1Total) == 8)
+    {
+        p1Done = true;
+    }
+    else
+    {
+        p1Done = false;
+    }
+    if((rowP2Total + colP2Total) == 8)
+    {
+        p2Done = true;
+    }
+    else
+    {
+        p2Done = false;
+    }
+    // if both true then it is a tie
+    if(p1Done && p2Done)
+    {
+        return 0.5;
+    }
+    // if p1Done is true and p2Done is false then player one has won
+    else if(p1Done && !p2Done)
+    {
+        return 1;
+    }
+    // if p1Done is false and p2Done is true then player two has won
+    else if(!p1Done && p2Done)
+    {
+        return 0;
+    }
+    // the game has not winner and there is no tie
+    else
+    {
+        return 3;
+    }
+}
+
+bool checkStatus(float status)
+{   // checks to see if the game has been solved or not.
+    if(status == -1)
+    {
+        cout << "\n" << "This board is invalid. Please provide a valid board." << endl;
+        return true;
+    }
+    else if(status == 1)
+    {
+        cout << "\n" << "Player 1 has already won the game." << endl;
+        return true;
+    }
+    else if(status == 0)
+    {
+        cout << "\n" << "Player 2 has already won the game." << endl;
+        return true;
+    }
+    else if(status == 0.5)
+    {
+        cout << "\n" << "The board is in a final tied game state" << endl;
+        return true;
+    }
+    else if(status == 3)
+    {
+        return false;
+    }
+}
+
 void minMax(vector <vector<string> > board)
 {// minMax algorithm
     float time;
     clock_t t;
     t = clock();
     // algorithm goes here
+
     t = clock() - t;
     time = (float(t)/CLOCKS_PER_SEC);
     printResults(time);
@@ -78,6 +213,7 @@ void alphaBetaPruning(vector <vector<string> > board)
 int main()
 {
     int choice = 0;
+    float status = 0;
     while(choice != 3)
     {
         cout << "Enter the corresponding number to choose one of the options below: " << endl;
@@ -107,8 +243,16 @@ int main()
             }
             cout << endl;
             printAndSaveBoard(file, board);
-            cout << endl;
-            minMax(board);
+            status = eval(board);
+            if(checkStatus(status))
+            {   // the game is either won or has an invalid board
+                cout << "Provide another board to be solved." << "\n" << endl;
+            }
+            else
+            {
+                cout << endl;
+                minMax(board);
+            }
 
         }
         else if (choice == 2)
@@ -127,8 +271,16 @@ int main()
             }
             cout << endl;
             printAndSaveBoard(file, board);
-            cout << endl;
-            alphaBetaPruning(board);
+            status = eval(board);
+            if(checkStatus(status))
+            {   // the game is either won or has an invalid board
+                cout << "Provide another board to be solved." << "\n" << endl;
+            }
+            else
+            {
+                cout << endl;
+                alphaBetaPruning(board);
+            }
         }
         else if (choice == 3)
         {// exit the program
